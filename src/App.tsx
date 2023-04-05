3e2bd52f7e4e425cf70cfb86a5a1f3dd437eaa1c
kyungchandevelop@gmail.com <championcom@naver.com>
2023-04-05 22:28:19 +0900

    board cross

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,19 @@ export default function App() {
   // 이 함수는 드랍이 일어날때 어떤일이 일어났는지에 대한 정보로 많은 args를 준다.
   // 드래그가 끝났을때 실행되는 함수아다.
   const onDragEnd = (args: DropResult) => {
-    console.log(args);
     const { destination, draggableId, source } = args;
+
+    // 사용자가 드랙그를 하려다가 하지 않았을때! 목적지가 없을때
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       //더이상 배열이 아니기 때문에 배열복사가 불가능하다
       //변화가 일어난 속성에 대해서만 복사한다.
       setTodo((oldToDos) => {
         const boardCopy = [...oldToDos[source.droppableId]]; //source.droppableId 카드의 index가 변경되기전 === 시작점id
         //1. 값의 index 에서 아이템을 지움
-        //  sourece.index 현재 item의 index!
+        // sourece.index 현재 item의 index!
         boardCopy.splice(source.index, 1);
-        //  목적지의 index 는 바꾸고자 하는 인덱스 , draggableId는 map으로 바인딩을 해주고 있기 때문에 id가 item들이 된다.
+        // 목적지의 index 는 바꾸고자 하는 인덱스 , draggableId는 map으로 바인딩을 해주고 있기 때문에 id가 item들이 된다.
         boardCopy.splice(Number(destination?.index), 0, args.draggableId);
         return {
           ...oldToDos,
@@ -34,9 +36,24 @@ export default function App() {
         };
       });
     }
-    // 요소의 목적직가 아니라면!
-    // 사용자가 드랙그를 하려닥가 하지 않았을때! 목적지가 없을때
-    if (!args.destination) return;
+
+    if (destination?.droppableId !== source.droppableId) {
+      //source.droppableId 시작점 //destination?.droppableId 목적지
+      setTodo((oldToDos) => {
+        const sourcBoard = [...oldToDos[source.droppableId]]; // 시작점 보드 복사
+        const destinationBoard = [...oldToDos[destination?.droppableId]]; // 목적지 보드 복사
+        //시작점 board에서 인덱스를 지워주고
+        sourcBoard.splice(source.index, 1);
+        // 시작점 board에서 잘라준 index를 목적지 모드에 더해준다.
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...oldToDos,
+          [source.droppableId]: sourcBoard,
+          [destination?.droppableId]: destinationBoard,
+        };
+      });
+    }
+
     // setTodo((oldTodo) => {
     //   console.log(args.source.index);
     //   const copyTodo = [...oldTodo]; //이전 배열은 복사한다.
